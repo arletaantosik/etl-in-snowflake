61dc00c3fa0a9116a84c6a35adf673bf2a3ef570
arletaantosik <162155471+arletaantosik@users.noreply.github.com>
2025-03-19 11:21:42 +0100

    Create copy-statement.sql

---
+++ sql/copy-statement.sql
@@ -0,0 +1,21 @@
+CREATE OR REPLACE TABLE FIRST_DB.PUBLIC.ORDERS (
+    ORDER_ID VARCHAR(30),
+    AMOUNT INT,
+    PROFIT INT,
+    QUANTITY INT,
+    CATEGORY VARCHAR(30),
+    SUBCATEGORY VARCHAR(30));
+
+SELECT * FROM FIRST_DB.PUBLIC.ORDERS;
+
+COPY INTO FIRST_DB.PUBLIC.ORDERS
+    FROM @MANAGE_DB.external_stages.aws_stage
+    file_format= (type = csv field_delimiter=',' skip_header=1)
+    files = ('OrderDetails.csv');
+
+LIST @MANAGE_DB.external_stages.aws_stage;    
+
+COPY INTO FIRST_DB.PUBLIC.ORDERS
+    FROM @MANAGE_DB.external_stages.aws_stage
+    file_format= (type = csv field_delimiter=',' skip_header=1)
+    pattern='.*Order.*';
