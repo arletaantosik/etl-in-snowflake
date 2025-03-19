496d3fb10f71398b78dcd782b2660b4757857c05
arletaantosik <162155471+arletaantosik@users.noreply.github.com>
2025-03-19 11:46:24 +0100

    Update copy-statement.sql

--- sql/copy-statement.sql
+++ sql/copy-statement.sql
@@ -67,3 +67,37 @@ COPY INTO FIRST_DB.PUBLIC.ORDERS_EX
     files=('OrderDetails.csv');
 
 SELECT * FROM FIRST_DB.PUBLIC.ORDERS_EX;
+
+CREATE OR REPLACE TABLE FIRST_DB.PUBLIC.ORDERS_EX (
+    ORDER_ID VARCHAR(30),
+    AMOUNT INT,
+    PROFIT INT,
+    PROFITABLE_FLAG VARCHAR(30)
+  
+    );    
+
+COPY INTO FIRST_DB.PUBLIC.ORDERS_EX (ORDER_ID,PROFIT)
+    FROM (select 
+            s.$1,
+            s.$3
+          from @MANAGE_DB.external_stages.aws_stage s)
+    file_format= (type = csv field_delimiter=',' skip_header=1)
+    files=('OrderDetails.csv');
+
+SELECT * FROM FIRST_DB.PUBLIC.ORDERS_EX;    
+
+CREATE OR REPLACE TABLE FIRST_DB.PUBLIC.ORDERS_EX (
+    ORDER_ID number autoincrement start 1 increment 1,
+    AMOUNT INT,
+    PROFIT INT,
+    PROFITABLE_FLAG VARCHAR(30)
+  
+    );
+
+COPY INTO FIRST_DB.PUBLIC.ORDERS_EX (PROFIT,AMOUNT)
+    FROM (select 
+            s.$2,
+            s.$3
+          from @MANAGE_DB.external_stages.aws_stage s)
+    file_format= (type = csv field_delimiter=',' skip_header=1)
+    files=('OrderDetails.csv');
