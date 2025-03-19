a34565914c58d438763a3a1152554ff8f97c477a
arletaantosik <162155471+arletaantosik@users.noreply.github.com>
2025-03-19 11:38:25 +0100

    Update copy-statement.sql

--- sql/copy-statement.sql
+++ sql/copy-statement.sql
@@ -19,3 +19,51 @@ COPY INTO FIRST_DB.PUBLIC.ORDERS
     FROM @MANAGE_DB.external_stages.aws_stage
     file_format= (type = csv field_delimiter=',' skip_header=1)
     pattern='.*Order.*';
+
+CREATE OR REPLACE TABLE FIRST_DB.PUBLIC.ORDERS_EX (
+ORDER_ID VARCHAR(30),
+AMOUNT INT
+);
+
+COPY INTO FIRST_DB.PUBLIC.ORDERS_EX
+    FROM (select s.$1, s.$2 from @MANAGE_DB.external_stages.aws_stage s)
+    file_format= (type = csv field_delimiter=',' skip_header=1)
+    files=('OrderDetails.csv');
+    
+SELECT * FROM FIRST_DB.PUBLIC.ORDERS_EX;
+
+CREATE OR REPLACE TABLE FIRST_DB.PUBLIC.ORDERS_EX (
+    ORDER_ID VARCHAR(30),
+    AMOUNT INT,
+    PROFIT INT,
+    PROFITABLE_FLAG VARCHAR(30)
+    );
+
+COPY INTO FIRST_DB.PUBLIC.ORDERS_EX
+FROM (select 
+        s.$1,
+        s.$2, 
+        s.$3,
+        CASE WHEN CAST(s.$3 as int) < 0 THEN 'not profitable' ELSE 'profitable' END 
+      from @MANAGE_DB.external_stages.aws_stage s)
+file_format= (type = csv field_delimiter=',' skip_header=1)
+files=('OrderDetails.csv');
+
+CREATE OR REPLACE TABLE FIRST_DB.PUBLIC.ORDERS_EX (
+    ORDER_ID VARCHAR(30),
+    AMOUNT INT,
+    PROFIT INT,
+    CATEGORY_SUBSTRING VARCHAR(5)
+    );
+
+COPY INTO FIRST_DB.PUBLIC.ORDERS_EX
+    FROM (select 
+            s.$1,
+            s.$2, 
+            s.$3,
+            substring(s.$5,1,5) 
+          from @MANAGE_DB.external_stages.aws_stage s)
+    file_format= (type = csv field_delimiter=',' skip_header=1)
+    files=('OrderDetails.csv');
+
+SELECT * FROM FIRST_DB.PUBLIC.ORDERS_EX;
