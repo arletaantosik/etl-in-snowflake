99d7ecb6bd4924c5de57f06171b00031d6703834
arletaantosik <162155471+arletaantosik@users.noreply.github.com>
2025-03-19 14:28:07 +0100

    Create copy-options.sql

---
+++ sql/copy-options.sql
@@ -0,0 +1,45 @@
+CREATE OR REPLACE DATABASE COPY_DB;
+
+CREATE OR REPLACE TABLE  COPY_DB.PUBLIC.ORDERS (
+    ORDER_ID VARCHAR(30),
+    AMOUNT VARCHAR(30),
+    PROFIT INT,
+    QUANTITY INT,
+    CATEGORY VARCHAR(30),
+    SUBCATEGORY VARCHAR(30));
+
+--files without errors
+CREATE OR REPLACE STAGE COPY_DB.PUBLIC.aws_stage_copy
+    url='s3://snowflakebucket-copyoption/size/';
+  
+LIST @COPY_DB.PUBLIC.aws_stage_copy;
+
+COPY INTO COPY_DB.PUBLIC.ORDERS
+    FROM @aws_stage_copy
+    file_format= (type = csv field_delimiter=',' skip_header=1)
+    pattern='.*Order.*'
+    VALIDATION_MODE = RETURN_ERRORS;
+
+COPY INTO COPY_DB.PUBLIC.ORDERS
+    FROM @aws_stage_copy
+    file_format= (type = csv field_delimiter=',' skip_header=1)
+    pattern='.*Order.*'
+   VALIDATION_MODE = RETURN_5_ROWS ;
+
+-- files with errors
+create or replace stage copy_db.public.aws_stage_copy
+    url ='s3://snowflakebucket-copyoption/returnfailed/';
+    
+list @copy_db.public.aws_stage_copy;
+
+copy into copy_db.public.orders
+    from @copy_db.public.aws_stage_copy
+    file_format = (type=csv field_delimiter=',' skip_header=1)
+    pattern='.*Order.*'
+    validation_mode=return_errors;
+
+copy into copy_db.public.orders
+    from @copy_db.public.aws_stage_copy
+    file_format = (type=csv field_delimiter=',' skip_header=1)
+    pattern='.*error.*'
+    validation_mode=return_1_rows;
