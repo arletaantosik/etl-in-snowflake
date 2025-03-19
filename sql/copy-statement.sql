c1bcdfc5bd61cdf77cef7fcc13171f4768f796eb
arletaantosik <162155471+arletaantosik@users.noreply.github.com>
2025-03-19 14:08:27 +0100

    Update copy-statement.sql

--- sql/copy-statement.sql
+++ sql/copy-statement.sql
@@ -148,3 +148,36 @@ COPY INTO FIRST_DB.PUBLIC.ORDERS_EX
     file_format= (type = csv field_delimiter=',' skip_header=1)
     files = ('OrderDetails_error.csv','OrderDetails_error2.csv')
     ON_ERROR = 'SKIP_FILE_3%'; 
+
+CREATE OR REPLACE TABLE FIRST_DB.PUBLIC.ORDERS_EX (
+    ORDER_ID VARCHAR(30),
+    AMOUNT INT,
+    PROFIT INT,
+    QUANTITY INT,
+    CATEGORY VARCHAR(30),
+    SUBCATEGORY VARCHAR(30));  
+
+
+CREATE OR REPLACE SCHEMA MANAGE_DB.file_formats;
+
+CREATE OR REPLACE file format MANAGE_DB.file_formats.my_file_format;
+
+DESC file format MANAGE_DB.file_formats.my_file_format;
+-- csv by default
+
+COPY INTO FIRST_DB.PUBLIC.ORDERS_EX
+    FROM @MANAGE_DB.external_stages.aws_stage_errorex
+    file_format= (FORMAT_NAME=MANAGE_DB.file_formats.my_file_format)
+    files = ('OrderDetails_error.csv')
+    ON_ERROR = 'SKIP_FILE_3'; 
+
+ALTER file format MANAGE_DB.file_formats.my_file_format
+    SET SKIP_HEADER = 1; -- 1 -> true
+
+
+CREATE OR REPLACE file format MANAGE_DB.file_formats.my_file_format
+    TYPE=JSON,
+    TIME_FORMAT=AUTO; 
+
+
+TRUNCATE table FIRST_DB.PUBLIC.ORDERS_EX;
