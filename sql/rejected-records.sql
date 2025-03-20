26524c952c36f9293dccaa0e88a2487e566444b4
arletaantosik <162155471+arletaantosik@users.noreply.github.com>
2025-03-20 15:55:35 +0100

    Update rejected-records.sql

--- sql/rejected-records.sql
+++ sql/rejected-records.sql
@@ -3,13 +3,6 @@ CREATE OR REPLACE STAGE COPY_DB.PUBLIC.aws_stage_copy
 
 LIST @COPY_DB.PUBLIC.aws_stage_copy;  
 
-
-COPY INTO COPY_DB.PUBLIC.ORDERS
-    FROM @aws_stage_copy
-    file_format= (type = csv field_delimiter=',' skip_header=1)
-    pattern='.*Order.*'
-    VALIDATION_MODE = RETURN_ERRORS;
-
 ---- 1) Saving rejected files after VALIDATION_MODE ---- 
 
 CREATE OR REPLACE TABLE  COPY_DB.PUBLIC.ORDERS (
@@ -26,7 +19,7 @@ COPY INTO COPY_DB.PUBLIC.ORDERS
     pattern='.*Order.*'
     VALIDATION_MODE = RETURN_ERRORS;
 
-// Storing rejected /failed results in a table
+-- Storing rejected /failed results in a table
 CREATE OR REPLACE TABLE rejected AS 
 select rejected_record from table(result_scan(last_query_id()));
 
@@ -43,7 +36,6 @@ COPY INTO COPY_DB.PUBLIC.ORDERS
     file_format= (type = csv field_delimiter=',' skip_header=1)
     pattern='.*Order.*'
     ON_ERROR=CONTINUE;
-
   
 select * from table(validate(orders, job_id => '_last'));
 
@@ -61,39 +53,16 @@ SPLIT_PART(rejected_record,',',5) as CATEGORY,
 SPLIT_PART(rejected_record,',',6) as SUBCATEGORY
 FROM rejected; 
 
-SELECT * FROM rejected_values;
-
 -- RETURN_FAILED_ONLY --
-CREATE OR REPLACE TABLE  COPY_DB.PUBLIC.ORDERS (
-    ORDER_ID VARCHAR(30),
-    AMOUNT VARCHAR(30),
-    PROFIT INT,
-    QUANTITY INT,
-    CATEGORY VARCHAR(30),
-    SUBCATEGORY VARCHAR(30));
-
-CREATE OR REPLACE STAGE COPY_DB.PUBLIC.aws_stage_copy
-    url='s3://snowflakebucket-copyoption/returnfailed/';
-  
-LIST @COPY_DB.PUBLIC.aws_stage_copy;
 
 COPY INTO COPY_DB.PUBLIC.ORDERS
     FROM @aws_stage_copy
     file_format= (type = csv field_delimiter=',' skip_header=1)
     pattern='.*Order.*'
     ON_ERROR =CONTINUE
-    RETURN_FAILED_ONLY = TRUE;
+    RETURN_FAILED_ONLY = TRUE; -- shows records with errors that weren't loaded
 
 -- Default = FALSE
-
-CREATE OR REPLACE TABLE  COPY_DB.PUBLIC.ORDERS (
-    ORDER_ID VARCHAR(30),
-    AMOUNT VARCHAR(30),
-    PROFIT INT,
-    QUANTITY INT,
-    CATEGORY VARCHAR(30),
-    SUBCATEGORY VARCHAR(30));
-
 
 COPY INTO COPY_DB.PUBLIC.ORDERS
     FROM @aws_stage_copy
