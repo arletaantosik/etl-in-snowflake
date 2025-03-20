9f8b27fc15264c3f47b993040c555e4926047e75
arletaantosik <162155471+arletaantosik@users.noreply.github.com>
2025-03-20 15:17:33 +0100

    Update copy-options.sql

--- sql/copy-options.sql
+++ sql/copy-options.sql
@@ -43,3 +43,26 @@ copy into copy_db.public.orders
     file_format = (type=csv field_delimiter=',' skip_header=1)
     pattern='.*error.*'
     validation_mode=return_1_rows;
+
+-- SIZE OPTION --
+
+CREATE OR REPLACE DATABASE COPY_DB;
+
+CREATE OR REPLACE TABLE  COPY_DB.PUBLIC.ORDERS (
+    ORDER_ID VARCHAR(30),
+    AMOUNT VARCHAR(30),
+    PROFIT INT,
+    QUANTITY INT,
+    CATEGORY VARCHAR(30),
+    SUBCATEGORY VARCHAR(30));
+
+CREATE OR REPLACE STAGE COPY_DB.PUBLIC.aws_stage_copy
+    url='s3://snowflakebucket-copyoption/size/';
+
+LIST @aws_stage_copy;   --size in B
+
+COPY INTO COPY_DB.PUBLIC.ORDERS
+    FROM @aws_stage_copy
+    file_format= (type = csv field_delimiter=',' skip_header=1)
+    pattern='.*Order.*'
+    SIZE_LIMIT=20000; -- only first file will pass even its size is bigger than 20KB
