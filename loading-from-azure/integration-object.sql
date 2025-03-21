ed0a27347d649a8c75e9c840c19ba313cf8a0569
arletaantosik <162155471+arletaantosik@users.noreply.github.com>
2025-03-21 15:45:24 +0100

    Create integration-object.sql

---
+++ loading-from-azure/integration-object.sql
@@ -0,0 +1,10 @@
+create or replace storage integration s3_int
+  TYPE = EXTERNAL_STAGE
+  STORAGE_PROVIDER = S3
+  ENABLED = TRUE 
+  STORAGE_AWS_ROLE_ARN = ''
+  STORAGE_ALLOWED_LOCATIONS = ('s3://<your-bucket-name>/<your-path>/', 's3://<your-bucket-name>/<your-path>/')
+   COMMENT = 'This an optional comment' ;
+
+--to fetch external_id to update it in the trust relationship in the role in AWS
+DESC integration s3_int;
