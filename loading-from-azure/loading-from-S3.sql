df15a0740edac31a75de0392e33106e8300cdba5
arletaantosik <162155471+arletaantosik@users.noreply.github.com>
2025-03-21 15:56:48 +0100

    Create loading-from-S3.sql

---
+++ loading-from-azure/loading-from-S3.sql
@@ -0,0 +1,15 @@
+CREATE OR REPLACE file format MANAGE_DB.file_formats.csv_fileformat
+    type = csv
+    field_delimiter = ','
+    skip_header = 1
+    null_if = ('NULL','null')
+    empty_field_as_null = TRUE    
+    FIELD_OPTIONALLY_ENCLOSED_BY = '"'   
+
+CREATE OR REPLACE stage MANAGE_DB.external_stages.csv_folder
+    URL = 's3://<your-bucket-name>/<your-path>/'
+    STORAGE_INTEGRATION = s3_int
+    FILE_FORMAT = MANAGE_DB.file_formats.csv_fileformat
+
+COPY INTO OUR_FIRST_DB.PUBLIC.movie_titles
+    FROM @MANAGE_DB.external_stages.csv_folder
