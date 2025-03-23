ba5936104e721b5aeb9e2aaba44a3cd6ec0de1ff
arletaantosik <162155471+arletaantosik@users.noreply.github.com>
2025-03-23 15:37:57 +0100

    Update creating-pipe.sql

--- snowpipe/creating-pipe.sql
+++ snowpipe/creating-pipe.sql
@@ -5,3 +5,8 @@ COPY INTO OUR_FIRST_DB.PUBLIC.employees
 FROM @MANAGE_DB.external_stages.csv_folder  ;
 
 DESC pipe employee_pipe;
+-- copy NOTIFICATION_CHANNEL to event notification section in AWS 
+-- prefix: csv/snowpipe/
+-- suffix -> can be used to limit certain format types 
+-- destination -> SQS queue 
+-- enter SQS queue ARN -> paste NOTIFICATION_CHANNEL
