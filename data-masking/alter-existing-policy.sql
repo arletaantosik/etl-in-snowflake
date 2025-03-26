f4d9c840bab879bb2e7a3fdf4d0a28c0349d98bb
arletaantosik <162155471+arletaantosik@users.noreply.github.com>
2025-03-26 18:48:44 +0100

    Create alter-existing-policy.sql

---
+++ data-masking/alter-existing-policy.sql
@@ -0,0 +1,9 @@
+USE ROLE ACCOUNTADMIN;
+
+alter masking policy phone set body ->
+case        
+ when current_role() in ('ANALYST_FULL', 'ACCOUNTADMIN') then val
+ else '**-**-**'
+ end;
+
+ALTER TABLE CUSTOMERS MODIFY COLUMN email UNSET MASKING POLICY;
