d9086e75baaec522589740a9850a6e60a16dd595
arletaantosik <162155471+arletaantosik@users.noreply.github.com>
2025-03-26 18:05:00 +0100

    Update creating.sql

--- materialized-views/creating.sql
+++ materialized-views/creating.sql
@@ -1,10 +1,18 @@
 -- if the data in the primary table is changing, the view also
 -- every time we run the view, we run the query on the table again
 -- use case:
-  -- query that takes a lot of time to be processed
-  -- more compute consumption
-  -- stores it like a 'normal table'
-  -- good performance
+    -- query that takes a lot of time to be processed and is used frequently
+    -- underlying data is not changing frequently and on a rather irregular basis
+          --> then streams or tasks could be cheaper and a better solution
+-- more compute consumption
+-- stores it like a 'normal table'
+-- good performance
+
+-- LIMITATION --
+  -- only for enterprise edition
+  -- no joins
+  -- limited amount of aggregation functions
+  -- no limit, having, order by clause
 
 ALTER SESSION SET USE_CACHED_RESULT=FALSE; -- disable global caching
 
