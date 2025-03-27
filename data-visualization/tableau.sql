a8a3a18d9f96fff6a150b287e114b636619c0741
arletaantosik <162155471+arletaantosik@users.noreply.github.com>
2025-03-27 13:34:00 +0100

    Update tableau.sql

--- data-visualization/tableau.sql
+++ data-visualization/tableau.sql
@@ -1,3 +1,7 @@
 -- connecting to snowflake from tableau
 -- steps:
 -- from tableau gui - connect to a server -> more... -> pick snowflake -> paste server name -> pick authentication method as 'Username and Password' and fill in
+
+-- connection
+  -- live - directly connect to data, speed depends of chosen warehouse
+  -- extract - import data into tableau's fast data engine, extract is created when you click on a sheet tab
