d1149051524f92c4bd03b1a5fb4604ac7dab6331
arletaantosik <162155471+arletaantosik@users.noreply.github.com>
2025-03-27 13:31:12 +0100

    Create tableau.sql

---
+++ data-visualization/tableau.sql
@@ -0,0 +1,3 @@
+-- connecting to snowflake from tableau
+-- steps:
+-- from tableau gui - connect to a server -> more... -> pick snowflake -> paste server name -> pick authentication method as 'Username and Password' and fill in
