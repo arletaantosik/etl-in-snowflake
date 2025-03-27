34fff2063b0bd76f250936468ba1b0bb7a188266
arletaantosik <162155471+arletaantosik@users.noreply.github.com>
2025-03-27 13:20:26 +0100

    Create powerbi.sql

---
+++ data-visualization/powerbi.sql
@@ -0,0 +1,8 @@
+-- connect with snowflake:
+-- in power bi:
+-- get data -> type snowflake -> server: xyz.snowflakecomputing.com (copy account url) warehouse: xyz
+-- -> type username and password
+
+-- -> connection setting: import or DirectQuery
+-- import - data processed locally in memory on our machine, not good for huge amount of data, local copy of data
+-- directquery -> compute power from snowflake (most of use cases)
