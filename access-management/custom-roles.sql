439526194a8244cc415c230e72aaa39beb525617
arletaantosik <162155471+arletaantosik@users.noreply.github.com>
2025-03-27 12:14:40 +0100

    Create custom-roles.sql

---
+++ access-management/custom-roles.sql
@@ -0,0 +1,13 @@
+-- customize roles to our needs and create own hierarchies
+-- custom roles are usually created by SECURITYADMIN and USERADMIN
+-- should be leading up to the SYSADMIN role (hierarchy)
+
+
+
+
+
+
+
+
+
+
